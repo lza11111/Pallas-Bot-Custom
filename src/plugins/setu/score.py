bcd39b1df038e93ffdf3459fa84e2ed92485ac44
cloud <12729828@qq.com>
2022-07-31 16:00:33 +0800

    fix

--- src/plugins/setu/score.py
+++ src/plugins/setu/score.py
@@ -3,7 +3,8 @@ from db.mongo import message_mongo, context_mongo, blacklist_mongo, score_mongo
 
 def query_score(user_id, group_id, score_type='setu_score'):
     msg_sum = message_mongo.count_documents({
-        '_id': user_id,
+        'user_id': user_id,
+        'group_id': group_id,
     })
 
     score_used = score_mongo.find_one(
