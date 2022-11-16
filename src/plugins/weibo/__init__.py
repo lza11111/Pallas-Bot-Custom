1793978e47f2c94097c8f20e08029084f5805a26
cloud <12729828@qq.com>
2022-11-17 01:08:47 +0800

    weibo

---
+++ src/plugins/weibo/__init__.py
@@ -0,0 +1,57 @@
+
+import random
+import asyncio
+import re
+import time
+import os
+import threading
+
+import nonebot
+from nonebot import on_message, require, get_bot, get_driver
+from nonebot.log import logger
+from nonebot.exception import ActionFailed
+from nonebot.typing import T_State
+from nonebot.rule import keyword, to_me, Rule
+from nonebot.adapters import Bot, Event
+from nonebot.adapters.onebot.v11 import GroupMessageEvent, PrivateMessageEvent
+from nonebot.adapters.onebot.v11 import permission, MessageSegment
+from nonebot.permission import Permission
+from nonebot.permission import SUPERUSER
+from src.common.config import BotConfig
+
+from .lib import weibo_extract, weibo_info_get, weibo_long_text, weibo_image_list
+
+global_config = nonebot.get_driver().config
+
+weibo = on_message(
+    priority=14,
+    block=True,
+    permission=permission.GROUP_ADMIN | permission.GROUP_OWNER | SUPERUSER
+)
+
+@weibo.handle()
+async def weibo_main(
+    bot: Bot, event: GroupMessageEvent, state: T_State
+):
+    if global_config.blocked_groups and event.group_id in global_config.blocked_groups:
+        return
+    message_str = event.get_plaintext()
+    
+    weibo_id = weibo_extract(message_str)
+
+    if not weibo_id:
+        return
+    weibo_info = await weibo_info_get(weibo_id)
+    weibo_text = None
+    if weibo_info:
+        logger.info(f'开始处理微博信息: {weibo_id}')
+        if weibo_info["isLongText"]:
+            weibo_text = await weibo_long_text(weibo_id)
+        else:
+            weibo_text = str(weibo_info['text_raw'])
+        
+        image_list = weibo_image_list(weibo_info)
+        finish = weibo_text or ''
+        for image in image_list:
+            finish += MessageSegment.image(file=image)
+        await weibo.finish(finish)
---
+++ src/plugins/weibo/lib.py
@@ -0,0 +1,47 @@
+import asyncio
+import re
+import httpx
+
+import nonebot
+from nonebot.log import logger
+
+global_config = nonebot.get_driver().config
+
+async def weibo_extract(text: str):
+    if "weibo" not in text:
+        return None
+    if not (weibo_id := re.compile(r"weibo.(?:cn|com)/\w+/(\d+|\w{9})").search(text)):
+        return None
+    return weibo_id[1]
+
+async def weibo_info_get(weibo_id):
+    url = f"https://weibo.com/ajax/statuses/show?id={weibo_id}"
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url)
+        try:
+            return resp.json()
+        except Exception as e:
+            logger.error(f"获取微博失败: {weibo_id} {e}")
+            return None
+
+async def weibo_long_text(weibo_id) -> str | None:
+    url = f"https://weibo.com/ajax/statuses/longtext?id={weibo_id}"
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url, cookies={"SUB": global_config.weibo_sub})
+        try:
+            json = resp.json()
+            if 'data' in json:
+                return json['data']['longTextContent']
+            return None
+        except Exception as e:
+            logger.error(f"获取长微博失败: {weibo_id} {e}")
+            return None
+
+def weibo_image_list(weibo_detail):
+    if 'pic_ids' not in weibo_detail or 'pic_infos' not in weibo_detail:
+        return []
+    pic_ids = weibo_detail['pic_ids']
+    pic_urls = []
+    for pic_id in pic_ids:
+        pic_urls.append(weibo_detail['pic_infos'][pic_id]['large']['url'])
+    return pic_urls
