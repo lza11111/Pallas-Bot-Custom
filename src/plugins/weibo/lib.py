dcfd74275e8e3d989c6c70ac2b5e49c94f96a315
cloud <12729828@qq.com>
2022-12-12 19:09:50 +0800

    add log

--- src/plugins/weibo/lib.py
+++ src/plugins/weibo/lib.py
@@ -76,6 +76,7 @@ async def deal_with_weibo(weibo_info) -> Union[str, Message, None]:
                 finish += await deal_with_weibo(weibo_info['retweeted_status']) or MessageSegment.text("\n获取转发微博失败")
             if 'page_info' in weibo_info and 'media_info' in weibo_info['page_info']:
                 finish += MessageSegment.text(f"\n视频: {weibo_info['page_info']['media_info']['stream_url_hd']}")
+            logger.info(finish)
             return finish
         except Exception as e:
             logger.exception(f'微博信息处理出错: {e}')
