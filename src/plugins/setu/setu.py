a5ad51ca9ee6050f9c0c1a313e2060c1f6fe7969
lza11111 <lza11111@163.com>
2023-01-24 14:40:47 +0800

    optimize

--- src/plugins/setu/setu.py
+++ src/plugins/setu/setu.py
@@ -12,12 +12,6 @@ async def request_one_setu(tag=None, r18=False):
     }).json()
     if response['error'] != '':
         return None
-    setu = response['data'][0]
-    print(setu)
-    image = None
-    async with httpx.AsyncClient() as client:
-        resp = await client.get(setu['urls']['original'])
-        if resp.status_code == 200:
-            image = resp.content
-    if image is not None :
-        return setu['title'] + MessageSegment.image(file=image)
+    setu = response['data'][0]['urls']['original']
+    return setu['title'] + MessageSegment.image(file=setu)
+        
