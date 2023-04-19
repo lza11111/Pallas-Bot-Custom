f389d078b52e710ce64e86a737888be3040462dc
cloud <12729828@qq.com>
2023-04-20 00:21:56 +0800

    replace trigger

--- src/plugins/xbox/__init__.py
+++ src/plugins/xbox/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from nonebot import on_command, get_driver
+from nonebot import on_message, get_driver
 from nonebot.log import logger
 from nonebot.typing import T_State
 from nonebot.adapters import Bot
@@ -18,7 +18,7 @@ global_config = get_driver().config
 
 tokens_file = Path(__file__).parent.parent.parent.parent.joinpath("tokens.json")
 
-xbox_status_wrapper = on_command("谁在摸鱼",
+xbox_status_wrapper = on_message(
     priority=14,
     block=False,
     permission=permission.GROUP_ADMIN | permission.GROUP_OWNER | SUPERUSER,
@@ -26,6 +26,9 @@ xbox_status_wrapper = on_command("谁在摸鱼",
 
 @xbox_status_wrapper.handle()
 async def xbox_status_wrapper_main(bot: Bot, event: GroupMessageEvent, state: T_State):
+    message_str = str(event.get_message())
+    if "谁在摸鱼" not in message_str:
+        return
     async with SignedSession() as session:
         auth_mgr = AuthenticationManager(session, global_config.aad_client_id, global_config.aad_client_secret, "")
         try:
