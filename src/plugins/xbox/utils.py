8b8b47a9c3f6c55b55e07ea535cee05d7b302456
cloud <12729828@qq.com>
2023-04-20 10:33:06 +0800

    update query

--- src/plugins/xbox/utils.py
+++ src/plugins/xbox/utils.py
@@ -1,10 +1,10 @@
 from db.mongo import group_member_mongo
 
-def query_member_qq_id(group_id: int, xuid: str) -> str:
-    result = group_member_mongo.aggregate([
-        {'$match': {
+def query_member_qq_id(group_id: int, xuid: str) -> str | None:
+    result = group_member_mongo.find({
             'group_id': group_id,
             'xuid': xuid
-        }},
-    ])
-    return result["user_id"]
+        })
+    if not result or result.count() == 0:
+        return None
+    return list(result)[0]['user_id']
