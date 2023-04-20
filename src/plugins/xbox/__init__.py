4f7fd9f25487cf9fb45e6bc55b6c1af0d8043d91
cloud <12729828@qq.com>
2023-04-20 23:40:42 +0800

    support push_user_status

--- src/plugins/xbox/__init__.py
+++ src/plugins/xbox/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from nonebot import on_message, get_driver
+from nonebot import on_message, get_driver, require, get_bot
 from nonebot.log import logger
 from nonebot.typing import T_State
 from nonebot.adapters import Bot
@@ -15,12 +15,28 @@ from xbox.webapi.authentication.manager import AuthenticationManager
 from xbox.webapi.authentication.models import OAuth2TokenResponse
 from xbox.webapi.common.signed_session import SignedSession
 
-from .utils import query_member_qq_id
+from .utils import query_member, update_member
 from src.common.utils import to_image
 
 global_config = get_driver().config
 
-tokens_file = Path(__file__).parent.parent.parent.parent.joinpath("tokens.json")
+tokens_file = Path(
+    __file__).parent.parent.parent.parent.joinpath("tokens.json")
+
+auth_mgr: AuthenticationManager
+
+
+async def get_all_people(xbl_client: XboxLiveClient):
+    profile_users = await xbl_client.people.get_friends_own_batch([auth_mgr.xsts_token.xuid])
+    my_profile = profile_users.people[0]
+
+    # Get friendslist
+    friendslist = await xbl_client.people.get_friends_own()
+    friendslist.people.append(my_profile)
+
+    friendslist.people.sort(key=lambda x: x.presence_state, reverse=True)
+
+    return friendslist.people
 
 xbox_status_wrapper = on_message(
     priority=14,
@@ -28,13 +44,109 @@ xbox_status_wrapper = on_message(
     permission=permission.GROUP_ADMIN | permission.GROUP_OWNER | SUPERUSER,
 )
 
+
 @xbox_status_wrapper.handle()
 async def xbox_status_wrapper_main(bot: Bot, event: GroupMessageEvent, state: T_State):
     message_str = str(event.get_message())
     if "谁在摸鱼" not in message_str:
         return
+    if not auth_mgr:
+        await refresh_tokens()
+
+    xbl_client = XboxLiveClient(
+        auth_mgr, language=DefaultXboxLiveLanguages.Hong_Kong)
+
+    # Get group member list
+    member_list = await bot.get_group_member_list(group_id=event.group_id)
+
+    friends_list = await get_all_people(xbl_client)
+
+    text = "看看谁在摸鱼:\n"
+
+    for friend in friends_list:
+        logger.info(friend)
+        member_bind = query_member(event.group_id, friend.xuid)
+        if member_bind is None:
+            continue
+        member = next((member for member in member_list if member["user_id"] == member_bind.user_id), None)
+        if member is None:
+            continue
+        nickname = member['card'] if member['card'] else member['nickname']
+        if friend.presence_details is None or len(friend.presence_details) == 0:
+            presence_text = 'Offline'
+            presence_icon = '🔴'
+            text += f"[{presence_icon}] {nickname} is {presence_text}\n"
+        else:
+            if any([details.state == 'Active' for details in friend.presence_details]):
+                presence_text = " and ".join(
+                    [f'{details.presence_text} on {details.device}' for details in friend.presence_details]) if friend.presence_details is not None else "None"
+                presence_icon = "🎮" if any(
+                    [details.is_game for details in friend.presence_details]) else "🟢"
+                text += f"{presence_icon} {nickname} is {friend.presence_text if friend.presence_text == 'Online' else f'playing {presence_text}'}\n"
+            else:
+                presence_text = friend.presence_text
+                presence_icon = '🔴'
+                text += f"{presence_icon} {nickname} {friend.presence_text}\n"
+
+    try:
+        await xbox_status_wrapper.finish(text)
+    except ActionFailed:
+        await xbox_status_wrapper.finish(MessageSegment.image(file=to_image(text)))
+
+schedule = require('nonebot_plugin_apscheduler').scheduler
+
+
+@schedule.scheduled_job('interval', seconds=5)
+async def push_user_status():
+    if not auth_mgr:
+        await refresh_tokens()
+    group_list = global_config.xbox_subscribe_group_list.split(',')
+
+    if len(group_list) == 0:
+        return
+    
+    xbl_client = XboxLiveClient(
+        auth_mgr, language=DefaultXboxLiveLanguages.Hong_Kong)
+    
+    for group in group_list:
+        member_list = await get_bot().get_group_member_list(group_id=group)
+        friends_list = await get_all_people(xbl_client)
+        for friend in friends_list:
+            text = ''
+            member_bind = query_member(group, friend.xuid)
+            if member_bind is None:
+                continue
+            member = next((member for member in member_list if member["user_id"] == member_bind.user_id), None)
+            if member is None:
+                continue
+
+            if friend.presence_text == member_bind.last_presence_text:
+                continue
+            nickname = member['card'] if member['card'] else member['nickname']
+            if friend.presence_details is None or len(friend.presence_details) == 0:
+                update_member(group, friend.xuid, member["user_id"], friend.presence_text)
+            else:
+                if any([details.state == 'Active' for details in friend.presence_details]):
+                    presence_text = " and ".join(
+                        [f'{details.presence_text} on {details.device}' for details in friend.presence_details]) if friend.presence_details is not None else "None"
+                    presence_icon = "🎮" if any(
+                        [details.is_game for details in friend.presence_details]) else "🟢"
+                    text += f"{presence_icon} {nickname} is {friend.presence_text if friend.presence_text == 'Online' else f'playing {presence_text}'}\n"
+                    update_member(group, friend.xuid, member["user_id"], friend.presence_text)
+                    await get_bot().call_api('send_group_msg', **{
+                        'message': text,
+                        'group_id': group})
+                else:
+                    presence_text = friend.presence_text
+                    presence_icon = '🔴'
+                    update_member(group, friend.xuid, member["user_id"], friend.presence_text)
+
+
+@schedule.scheduled_job('interval', minutes=30)
+async def refresh_tokens():
     async with SignedSession() as session:
-        auth_mgr = AuthenticationManager(session, global_config.aad_client_id, global_config.aad_client_secret, "")
+        auth_mgr = AuthenticationManager(
+            session, global_config.aad_client_id, global_config.aad_client_secret, "")
         try:
             with open(tokens_file) as f:
                 tokens = f.read()
@@ -59,44 +171,3 @@ async def xbox_status_wrapper_main(bot: Bot, event: GroupMessageEvent, state: T_
         # Save the refreshed/updated tokens
         with open(tokens_file, mode="w") as f:
             f.write(auth_mgr.oauth.json())
-
-        xbl_client = XboxLiveClient(auth_mgr, language=DefaultXboxLiveLanguages.Hong_Kong)
-        
-        # Get group member list
-        member_list = await bot.get_group_member_list(group_id=event.group_id)
-
-        # Get profile
-        profile_users = await xbl_client.people.get_friends_own_batch([auth_mgr.xsts_token.xuid])
-        my_profile = profile_users.people[0]
-        
-        # Get friendslist
-        friendslist = await xbl_client.people.get_friends_own()
-        friendslist.people.append(my_profile)
-        text = "看看谁在摸鱼:\n"
-        count = 0
-        friendslist.people.sort(key=lambda x: x.presence_state, reverse=True)
-        for friend in friendslist.people:
-            logger.info(friend)
-            for member in member_list:
-                if member["user_id"] == query_member_qq_id(event.group_id, friend.xuid):
-                    nickname = member['card'] if member['card'] else member['nickname']
-                    if friend.presence_details is None or len(friend.presence_details) == 0:
-                        presence_text = 'Offline'
-                        presence_icon = '🔴'
-                        text += f"[{presence_icon}] {nickname} is {presence_text}\n"
-                    else:
-                        if any([details.state == 'Active' for details in friend.presence_details]):
-                            presence_text = " and ".join([f'{details.presence_text} on {details.device}' for details in friend.presence_details]) if friend.presence_details is not None else "None"
-                            presence_icon = "🎮" if any([details.is_game for details in friend.presence_details]) else "🟢"
-                            text += f"{presence_icon} {nickname} is {friend.presence_text if friend.presence_text == 'Online' else f'playing {presence_text}'}\n"
-                        else:
-                            presence_text = friend.presence_text
-                            presence_icon = '🔴'
-                            text += f"{presence_icon} {nickname} {friend.presence_text}\n"
-                    count += 1
-        if count == 0:
-            text = "没人在摸鱼"
-        try:
-            await xbox_status_wrapper.finish(text)
-        except ActionFailed:
-            await xbox_status_wrapper.finish(MessageSegment.image(file=to_image(text)))
--- src/plugins/xbox/utils.py
+++ src/plugins/xbox/utils.py
@@ -1,11 +1,36 @@
 from typing import Union
 from db.mongo import group_member_mongo
 
-def query_member_qq_id(group_id: int, xuid: str) -> Union[str,None]:
+class GroupMemberStatus:
+    group_id: int
+    xuid: str
+    user_id: int
+    last_presence_text: str
+
+def query_member(group_id: int, xuid: str) -> Union[GroupMemberStatus,None]:
     result = group_member_mongo.find({
             'group_id': group_id,
             'xuid': xuid
         })
     if not result or result.count() == 0:
         return None
-    return list(result)[0]['user_id']
+    return list(result)[0]
+
+def query_member_list(group_id: int) -> list:
+    result = group_member_mongo.find({
+            'group_id': group_id
+        })
+    if not result or result.count() == 0:
+        return []
+    return list(result)
+
+def update_member(group_id: int, xuid: str, user_id: int, last_presence_text: str):
+    group_member_mongo.update_one({
+        'group_id': group_id,
+        'xuid': xuid,
+        'user_id': user_id,
+    }, {
+        '$set': {
+            'last_presence_text': last_presence_text
+        }
+    }, upsert=True)
