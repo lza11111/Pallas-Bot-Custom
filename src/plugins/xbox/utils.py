207c2990a674b29a1ef8d3b2838d2e090f0c355f
cloud <12729828@qq.com>
2023-04-20 10:37:16 +0800

    fix type

--- src/plugins/xbox/utils.py
+++ src/plugins/xbox/utils.py
@@ -1,6 +1,7 @@
+from typing import Union
 from db.mongo import group_member_mongo
 
-def query_member_qq_id(group_id: int, xuid: str) -> str | None:
+def query_member_qq_id(group_id: int, xuid: str) -> Union[str,None]:
     result = group_member_mongo.find({
             'group_id': group_id,
             'xuid': xuid
